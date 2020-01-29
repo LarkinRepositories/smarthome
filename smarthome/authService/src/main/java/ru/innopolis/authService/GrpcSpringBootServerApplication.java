9599f2755074161c6fb44b4df3f9dabb46bb2ec2
Sergey Molodtsov <Molodtsov.S.Dm@sberbank.ru>
2020-01-29 11:04:23 +0500

    some bug fix & gradle

---
+++ smarthome/authService/src/main/java/ru/innopolis/authService/GrpcSpringBootServerApplication.java
@@ -0,0 +1,15 @@
+package ru.innopolis.authService;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringBootServerApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcSpringBootServerApplication.class, args);
+    }
+
+}
