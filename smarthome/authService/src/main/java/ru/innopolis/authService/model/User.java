86a3ca6a817eb7608f9659809c7668f94edcd12b
larkinds <larkindsx@gmail.com>
2020-02-29 14:05:16 +0700

    исправил пользователя - добавлены поля chatId и phone, дополнен файл лога liquibase

--- smarthome/authService/src/main/java/ru/innopolis/authService/model/User.java
+++ smarthome/authService/src/main/java/ru/innopolis/authService/model/User.java
@@ -3,6 +3,7 @@ package ru.innopolis.authService.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import java.util.List;
 
 @Entity
@@ -15,16 +16,19 @@ public class User extends BaseEntity {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+    @Email
     @Column(name = "email")
     private String email;
     @Column(name = "password")
     private String password;
-//    @Column(name = "chat_id")
-//    private long chatId;
+    @Column(name = "chat_id")
+    private long chatId;
+    @Column(name = "phone")
+    private long phone;
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable (name = "user_roles",
             joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id") },
             inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
     private List<Role> roles;
-//    private List<Object> devices;
+
 }
