b138358ea1abaa20392334fd179771d322e6c59b
Mustafina Alina <sabirzyanova_a@mail.ru>
2020-03-01 13:40:27 +0300

    edit EnableEurekaClient

--- smarthome/smsSendService/src/main/java/ru/innopolis/sms/SmsApplication.java
+++ smarthome/smsSendService/src/main/java/ru/innopolis/sms/SmsApplication.java
@@ -2,15 +2,18 @@ package ru.innopolis.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Точка входа в приложение
  * @author Alina
  * пример вызова
  * http://localhost:8080/sendSMS?to=%2BXXXXXXXXXXX&message=Hello
+ * http://localhost:8762/sms/sendSMS?to=%2BXXXXXXXXXXX&message=Hello
  * %2BXXXXXXXXXXX - номер телефона в международном формате (%2B-знак "+")
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmsApplication.class, args);
